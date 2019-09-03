af72b954781e2f80d5354993dab4cdd21a175741
Mehmet Ceylan <mehmetceylan1023@gmail.com>
2019-09-03 10:46:56 +0300

    .

---
+++ Sy.Core/Abstracts/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sy.Core.Abstracts
+{
+  public interface IEntity
+    {
+
+    }
+
+    public interface IEntity<IKey>
+    {
+        IKey Id { get; set; }
+    }
+}
